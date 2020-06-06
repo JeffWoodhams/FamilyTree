ff796b8ea7cde894545db6335aff4a13a6995588
Jeff Woodhams <jeff.woodhams@argentaconsult.com>
2020-06-06 09:22:26 +0100

    Publish 6/6

--- src/app/people-list/people-list.component.ts
+++ src/app/people-list/people-list.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit} from '@angular/core';
-import { Person } from 'sdk';
-import { Router } from '@angular/router';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Event, Person } from 'sdk';
+import { ActivatedRoute, Router } from '@angular/router';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
 import * as peopleData from '../../assets/People.json'
-
 @Component({
   selector: 'people-list',
   templateUrl: './people-list.component.html',
@@ -15,18 +15,19 @@ export class PeopleListComponent implements OnInit{
   displayedColumns = ["name", "birthYear"];
   public people = new MatTableDataSource<Person>(); 
 
-  constructor(private router: Router){}
+  constructor(private route: ActivatedRoute, private router: Router, private dialog: MatDialog){}
 
 
   ngOnInit(): void {
-    this.people = (peopleData as any).default;
-    this.people.filterPredicate = (data: Person, filter: string) => data.name.toLowerCase().startsWith(filter);
+    this.people.filterPredicate = (data: Person, filter: string) => {
+      return data.personID.toLowerCase().startsWith(filter);
+     };
+    this.people.data = (peopleData as any).default;
   }
 
   openFamilyChart(personID: string) {
     this.router.navigate(['family-chart/',personID]);
   }
-
   public doFilter = (value: string) => {
     this.people.filter = value.toLowerCase();
   }
